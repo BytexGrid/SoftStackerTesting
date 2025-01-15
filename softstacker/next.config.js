6ce0b5c1c99662b39c294efa1e4051b64d08ac28
BytexGrid <dnalinkman@outlook.com>
2025-01-15 15:03:02 +0530

    f8

--- softstacker/next.config.js
+++ softstacker/next.config.js
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   images: {
-    unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'api.dicebear.com',
-        pathname: '/7.x/**',
-      },
-    ],
-  },
-  assetPrefix: '/SoftStacker/',
-  basePath: '/SoftStacker'
+    unoptimized: true
+  }
 }
 
 module.exports = nextConfig 
